07749d2fd657c15be4acc23e055dcf8262490af1
NychyporukR <131202862+NychyporukR@users.noreply.github.com>
2023-12-10 13:38:04 +0200

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "Enter element at position (" << i << ", " << j << "): ";
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "Entered matrix:\n";
+    for (const auto& row : matrix) {
+        for (int element : row) {
+            cout << element << " ";
+        }
+        cout << endl;
+    }
+
+    // Використання алгоритму count_if для обчислення кількості нульових елементів
+    int zeroCount = count_if(matrix.begin(), matrix.end(),
+        [](const vector<int>& row) {
+            return count(row.begin(), row.end(), 0) > 0;
+        });
+
+    cout << "Number of zero elements: " << zeroCount << endl;
+
+    return 0;
+}
